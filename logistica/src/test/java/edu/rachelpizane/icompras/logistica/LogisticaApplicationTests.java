359de181eafc70b07f3e1b163245d1e5faff6246
Rachel Pizane <rachel.maia@dbserver.com.br>
2026-05-20 10:56:31 -0300

    feat: inicializar serviço de logistica

---
+++ logistica/src/test/java/edu/rachelpizane/icompras/logistica/LogisticaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.rachelpizane.icompras.logistica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
